78c4ce1484e91c7ede71c9981562476fd085d330
Quinn Barber <qbarber2020@gmail.com>
2022-10-23 13:56:14 -0400

    Refactor usability tests.

--- src/SignUp/SignUp.jsx
+++ src/SignUp/SignUp.jsx
@@ -1,8 +1,34 @@
+import ViewObject from '../ViewObject/ViewObject';
 import React, {Component} from 'react';
 import './SignUp.css';
 
 export default class SignUp extends Component{
-    
+
+    constructor(props) {
+        super(props);
+        this.canvasRef = React.createRef();
+    }
+
+    // ******************* COMPONENT LIFECYCLE ******************* //
+    componentDidMount() {
+        // Get canvas, pass to custom class
+        const canvas = this.canvasRef.current;
+        this.viewOjbect = new ViewObject(canvas);
+
+        // Init any event listeners
+        window.addEventListener('mousemove', this.mouseMove);
+        window.addEventListener('resize', this.handleResize);
+    }
+
+    // ******************* EVENT LISTENERS ******************* //
+    mouseMove = (event) => {
+        this.viewOjbect.onMouseMove();
+    }
+
+    handleResize = () => {
+        this.viewOjbect.onWindowResize(window.innerWidth, window.innerHeight);
+    };
+
     render(){
         // Forgot password, e-mail verification
         return(
@@ -22,6 +48,9 @@ export default class SignUp extends Component{
                     <button>signup</button>
                     <br/><br/>
                     <text>Already Registered? </text><a href='/login'>Login</a>
+                </div>
+                <div className="object">
+                    <canvas ref={this.canvasRef} />
                 </div>
             </div>
         );
